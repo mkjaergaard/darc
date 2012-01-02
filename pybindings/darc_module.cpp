cf32786973b9d7bbfc2e838b8f971590f38e924e
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-01-01 18:56:06 -0800

    initial darc pybindings for node and component

--- pybindings/darc_module.cpp
+++ pybindings/darc_module.cpp
@@ -2,6 +2,11 @@
 #include <boost/thread.hpp>
 #include <iostream>
 
+#include <darc/node.h>
+#include <darc/node_impl.h>
+#include <darc/component_register.h>
+#include <darc/component.h>
+
 #define SHOW() std::cout << __PRETTY_FUNCTION__ << "\n"
 
 struct Thing
@@ -47,6 +52,19 @@ namespace bp = boost::python;
 BOOST_PYTHON_MODULE(darc)
 {
   SHOW();
+
+  bp::class_<darc::Component, darc::ComponentPtr, boost::noncopyable>("Component", bp::no_init)
+    .def("run", &darc::Component::run);
+
+  bp::class_<darc::ComponentRegister>("ComponentRegister", bp::no_init)
+    .def("instantiateComponent", &darc::ComponentRegister::instantiateComponent)
+    .staticmethod("instantiateComponent");
+
+  bp::class_<darc::Node, darc::NodePtr, boost::noncopyable>("Node", bp::no_init)
+    .def("create", &darc::Node::create)
+    .staticmethod("create");
+
+  bp::class_<darc::NodeImpl, boost::noncopyable>("NodeImpl", bp::no_init);
 
   bp::class_<Thing, boost::noncopyable>("Thing")
     .def("start", &Thing::start)
---
+++ pybindings/darc_node.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import darc, time, publisher_component
+
+n = darc.Node.create()
+c = darc.ComponentRegister.instantiateComponent("MyPublisherComponent", n)
+c.run()
+
+from IPython.Shell import IPShellEmbed
+ipshell = IPShellEmbed()
+ipshell()
