7f493c0885d5e2af633e7ce97eb3423a962147dc
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-01-04 22:19:49 -0800

    moving sources around

---
+++ test/test_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import darc, time, publisher_component
+
+n = darc.Node.create()
+c = darc.ComponentRegister.instantiateComponent("MyPublisherComponent", n)
+
+n.setNodeID(120)
+#n.accept("udp://127.0.0.1:5120")
+#n.connect(125, "udp://127.0.0.1:5125")
+
+#c.run()
+
+from IPython.Shell import IPShellEmbed
+ipshell = IPShellEmbed()
+ipshell()
