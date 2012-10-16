670a8c75a1fc5394489491d3d7e377741209df24
Morten Kjaergaard <morten.kjargaard@prevas.dk>
2012-10-16 16:23:28 +0200

    added peer class

--- include/darc/peer.hpp
+++ include/darc/peer.hpp
@@ -4,35 +4,41 @@
 #include <boost/bind.hpp>
 #include <darc/buffer/shared_buffer.hpp>
 #include <darc/id.hpp>
-#include <darc/peer.hpp>
 
 namespace darc
 {
 
 typedef uint32_t service_type;
 
-class peer_service
+class peer
 {
 protected:
   typedef boost::function<void(const darc::ID&,
 			       service_type,
 			       darc::buffer::shared_buffer)> send_to_function_type;
-  peer& peer_;
+
+  send_to_function_type send_to_function_;
+  ID id_;
 
 public:
-  virtual void recv(const darc::ID& src_peer_id,
-		    service_type,
-		    darc::buffer::shared_buffer data) = 0;
-
-  peer_service(darc::peer& p) :
-    peer_(p)
+  peer() :
+    id_(ID::create())
   {}
 
   void send_to(const ID& peer_id, service_type service, darc::buffer::shared_buffer data)
   {
-    peer_.send_to(peer_id, service, data);
+    send_to_function_(peer_id, service, data);
   }
 
+  virtual void set_send_to_function(send_to_function_type send_to_function)
+  {
+    send_to_function_ = send_to_function;
+  }
+
+  const ID& id()
+  {
+    return id_;
+  }
 
 };
 
